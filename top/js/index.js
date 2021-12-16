cb6cbd3d3a5b100077754e0786edf9af1d846f66
Quang Trương <quangklynk@gmail.com>
2021-12-16 20:28:01 +0700

    add animation scroll to tag

--- top/js/index.js
+++ top/js/index.js
@@ -2,3 +2,15 @@ $(window).on("load scroll", function () {
 
 });
 
+$(document).ready(function () {
+    $('a[href*=\\#]:not([href=\\#])').on('click', function () {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.substr(1) + ']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    });
+  });
